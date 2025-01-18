709d631f02f5fe0814c7d788e6afc8562cc38301
SonPhamBOW <2k3sonpham@gmail.com>
2025-01-18 22:18:13 +0700

    Check git account

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        hello everyone my name is son
+        hello everyone my name is son sd
       </p>
     </>
   )
